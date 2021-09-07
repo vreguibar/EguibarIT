aa210e2b810e72ce190d12e6a11151faad80d248
Vicente Rodriguez Eguibar <38666475+vreguibar@users.noreply.github.com>
2021-09-07 19:07:56 +0200

    Function was copied incorrectly from the single file. The correct function is now within the file.

--- Public/New-DelegatedAdOU.ps1
+++ Public/New-DelegatedAdOU.ps1
@@ -1,83 +1,118 @@
-function New-DelegateAdGpo
+function New-DelegateAdOU
 {
     <#
         .Synopsis
-            Creates and Links new GPO
-            
+            Create New custom delegated AD OU
         .DESCRIPTION
-            Create new custom delegated GPO, Delegate rights to an existing group and links it to the given OU
-            
+            Create New custom delegated AD OU, and remove
+            some groups as Account Operators and Print Operators
         .EXAMPLE
-            New-DelegateAdGpo MyNewGPO C "OU=Servers,OU=eguibarit,OU=local" "SL_GpoRight"
-        .EXAMPLE
-            New-DelegateAdGpo -gpoDescription MyNewGPO -gpoScope C -gpoLinkPath "OU=Servers,OU=eguibarit,OU=local" -GpoAdmin "SL_GpoRight"
-            
-        .PARAMETER gpoDescription
-            [STRING] Description of the GPO. Used to build the name. Only Characters a-z A-Z
-        .PARAMETER gpoScope
-            [STRING] Scope of the GPO. U for Users and C for Computers DEFAULT is U. The non-used part of the GPO will get disabled
-        .PARAMETER gpoLinkPath
-            [STRING] Where to link the newly created GPO
-        .PARAMETER GpoAdmin
-            [STRING] Domain Local Group with GPO Rights to be assigned
+            New-DelegateAdOU OuName OuPath OuDescription ...
+        .INPUTS
+            Param1  OuName:............ [STRING] Name of the OU
+            Param2  OuPath:............ [STRING] LDAP path where this ou will be created
+            Param3  OuDescrition:...... [STRING] Full description of the OU
+            Param4  OuCity:............ [STRING] 
+            Param5  OuCountry:......... [STRING] 
+            Param6  OuStreetAddress:... [STRING] 
+            Param7  OuState:........... [STRING] 
+            Param8  OuZipCode:......... [STRING] 
+            Param9  strOuDisplayName:.. [STRING] 
+            Param10 RemoveAuthenticatedUsers:.. [Switch] Remove Authenticated Users
+            Param11 CleanACL:.......... [Switch] Remove Authenticated Users
 
             No Config.xml needed for this function.
-            
-        .INPUTS
-            None
-            
-        .OUTPUTS
-            Microsoft.GroupPolicy.Gpo
-            
-        .LINKS
-            http://www.eguibarit.com
-        
+
         .NOTES
             Version:         1.2
-            DateModified:    22/Jan/2019
+            DateModified:    01/Feb/2017
             LasModifiedBy:   Vicente Rodriguez Eguibar
                 vicente@eguibar.com
                 Eguibar Information Technology S.L.
                 http://www.eguibarit.com
     #>
     [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Medium')]
-    [OutputType([Microsoft.GroupPolicy.Gpo])]
+
+    # https://docs.microsoft.com/en-us/dotnet/api/microsoft.activedirectory.management?view=activedirectory-management-10.0
+    [OutputType([Microsoft.ActiveDirectory.Management.ADOrganizationalUnit])]
+    
     Param
     (
-        # Param1 GPO description, used to generate name
+        # Param1 Site Name
         [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, ValueFromRemainingArguments = $false,
-            HelpMessage = 'Description of the GPO. Used to build the name.',
-        Position = 0)]
+            HelpMessage = 'Name of the OU',
+            Position = 0)]
         [ValidateNotNull()]
         [ValidateNotNullOrEmpty()]
+        [ValidateLength(2,50)]
         [string]
-        $gpoDescription,
+        $ouName,
 
-        # Param2 GPO scope. U = Users, C = Computers
+        # Param2 OU DistinguishedName (Path)
         [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, ValueFromRemainingArguments = $false,
-            HelpMessage = 'Scope of the GPO. U for Users and C for Computers DEFAULT is U. The non-used part of the GPO will get disabled',
-        Position = 1)]
-        [ValidateSet('U', 'C', ignorecase = $false)]
-        [string]
-        $gpoScope,
-
-        # Param3 GPO Link to OU
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, ValueFromRemainingArguments = $false,
-            HelpMessage = 'Where to link the newly created GPO',
-        Position = 2)]
-        [ValidateNotNull()]
+            HelpMessage = 'LDAP path where this ou will be created',
+            Position = 1)]
         [ValidateNotNullOrEmpty()]
         [string]
-        $gpoLinkPath,
-        
-        # Param4 Domain Local Group with GPO Rights to be assigned
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, ValueFromRemainingArguments = $false,
-            HelpMessage = 'Domain Local Group with GPO Rights to be assigned',
-        Position = 3)]
-        [string]
-        $GpoAdmin
+        $ouPath,
 
-    )
+        # Param3 OU Description
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, ValueFromRemainingArguments = $false,
+            Position = 2)]
+        [string]
+        $ouDescription,
+
+        # Param4 OU City
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, ValueFromRemainingArguments = $false,
+            Position = 3)]
+        [string]
+        $ouCity,
+
+        # Param5 OU Country
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, ValueFromRemainingArguments = $false,
+            Position = 4)]
+        [string]
+        $ouCountry,
+
+        # Param6 OU Street Address
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, ValueFromRemainingArguments = $false,
+            Position = 5)]
+        [string]
+        $ouStreetAddress,
+
+        # Param7 OU State
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, ValueFromRemainingArguments = $false,
+            Position = 6)]
+        [string]
+        $ouState,
+
+        # Param8 OU Postal Code
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, ValueFromRemainingArguments = $false,
+            Position = 7)]
+        [string]
+        $ouZIPCode,
+
+        # Param9 OU Display Name
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, ValueFromRemainingArguments = $false,
+        Position = 8)]
+        [string]
+        $strOuDisplayName,
+
+        #PARAM10 Remove Authenticated Users
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, ValueFromRemainingArguments = $false,
+            HelpMessage = 'Remove Authenticated Users. CAUTION! This might affect applying GPO to objects.',
+        Position = 9)]
+        [switch]
+        $RemoveAuthenticatedUsers,
+
+        #PARAM11 Remove Specific Non-Inherited ACE and enable inheritance
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, ValueFromRemainingArguments = $false,
+            HelpMessage = ' Remove Specific Non-Inherited ACE and enable inheritance.',
+        Position = 10)]
+        [switch]
+        $CleanACL
+
+  )
 
     Begin
     {
@@ -91,118 +126,93 @@ function New-DelegateAdGpo
         [string]$pb = ($PSBoundParameters | Format-Table -AutoSize | Out-String).TrimEnd()
         Write-Verbose -Message "Parameters used by the function... $NL$($pb.split($NL).Foreach({"$($HTab*4)$_"}) | Out-String) $NL"
 
+    
+        Import-Module -name EguibarIT.Delegation -Verbose:$false
 
-        Import-Module -name ActiveDirectory -Verbose:$false
-        Import-Module -name GroupPolicy     -Verbose:$false
+        #------------------------------------------------------------------------------
+        # Define the variables
 
         try
         {
-            # Active Directory Domain Distinguished Name
-            If(-not (Test-Path -Path variable:AdDn))
-            {
-                New-Variable -Name 'AdDn' -Value ([ADSI]'LDAP://RootDSE').rootDomainNamingContext.ToString() -Option ReadOnly -Force
-            }
-        }
-        catch { throw }
-        
-        
-        $gpoAlreadyExist = $null
-        $gpoName = '{0}-{1}' -f $PSBoundParameters['gpoScope'], $PSBoundParameters['gpoDescription']
-        #$adGroupName = Get-ADGroup -Identity $GpoAdmin
-        $dcServer = (Get-ADDomaincontroller).HostName
-    }
-    Process
-    {
-        # Check if the GPO already exist
-        $gpoAlreadyExist = Get-GPO -Name $gpoName -ErrorAction SilentlyContinue
-        # Clean the error if object does not exist. No need to log.
-        $error.clear()
-
-        if (-not $gpoAlreadyExist)
-        {
-          Write-Verbose -Message ('Policy: Create policy {0}' -f $gpoName)
-          $parameters = @{
-            Name        = $gpoName
-            Comment     = $gpoName
-            Server      = $dcServer
-            ErrorAction = 'SilentlyContinue'
-            Verbose     = $true
-          }
-          $CurrentNewGPO = New-GPO @parameters
-
-
-          Write-Verbose -Message '1 second pause to give AD a chance to catch up'
-          Start-Sleep -Seconds 1
-
-          #Write-Host "Remove Authenticated Users from GPO Security Filtering"
-          #Set-GPPermissions -Name $gpoName -PermissionLevel None -TargetName "Authenticated Users" -TargetType Group -Server $dcServer
-
-          # Give Rights to SL_AdRights
-          Write-Verbose -Message ('Add Administrators to {0}' -f $gpoName)
-          $parameters = @{
-            GUID            = $CurrentNewGPO.Id
-            PermissionLevel = 'GpoEditDeleteModifySecurity'
-            TargetName      = $GpoAdmin
-            TargetType      = 'group'
-            Server          = $dcServer
-            ErrorAction     = 'SilentlyContinue'
-            Verbose     = $true
-          }
-          Set-GPPermissions @parameters
-
-
-          #Write-Host "Add Editors to GPO"
-          #Set-GPPermissions -Name $gpoName -PermissionLevel GpoEdit -TargetName $gpoEditors -TargetType group -Server $dcServer
-
-          #Write-Host "Add AD-Group to Security Filtering on GPO"
-          #Set-GPPermissions -Name $gpoName -PermissionLevel GpoApply -TargetName "$($adGroupName)" -TargetType Group -Server $dcServer
-          If ($gpoScope -eq 'C')
+          # Active Directory Domain Distinguished Name
+          If(-not (Test-Path -Path variable:AdDn))
           {
-            Write-Verbose -Message 'Disable Policy User Settings'
-            $CurrentNewGPO.GpoStatus = 'UserSettingsDisabled'
+            New-Variable -Name 'AdDn' -Value ([ADSI]'LDAP://RootDSE').rootDomainNamingContext.ToString() -Option ReadOnly -Force
+          }
+
+          # Sites OU Distinguished Name
+          $ouNameDN = 'OU={0},{1}' -f $PSBoundParameters['ouName'], $PSBoundParameters['ouPath']
+        }
+        Catch
+        {
+          throw
+        }
+
+        $Return = $null
+
+        # END variables
+        #------------------------------------------------------------------------------
+    }
+
+      Process
+      {
+        # 
+        if (-not $strOuDisplayName)
+        {
+          $strOuDisplayName = $PSBoundParameters['ouName']
+        }
+
+        try
+        {
+          # Try to get Ou
+          $OUexists = Get-AdOrganizationalUnit -Filter { distinguishedName -eq $ouNameDN } -SearchBase $AdDn
+          
+          # Check if OU exists
+          If($OUexists)
+          {
+            # OU it does exists
+            Write-Warning -Message ('Organizational Unit {0} already exists.' -f $ouNameDN)
           }
           else
           {
-            Write-Verbose -Message 'Disable Policy Computer Settings'
-            $CurrentNewGPO.GpoStatus = 'ComputerSettingsDisabled'
+            Write-Verbose -Message ('Creating the {0} Organizational Unit' -f $PSBoundParameters['ouName'])
+            # Create OU
+            $parameters = @{
+              Name                            = $PSBoundParameters['ouName']
+              Path                            = $PSBoundParameters['ouPath']
+              City                            = $PSBoundParameters['ouCity']
+              Country                         = $PSBoundParameters['ouCountry']
+              Description                     = $PSBoundParameters['ouDescription']
+              DisplayName                     = $PSBoundParameters['strOuDisplayName']
+              PostalCode                      = $PSBoundParameters['ouZIPCode']
+              ProtectedFromAccidentalDeletion = $true
+              StreetAddress                   = $PSBoundParameters['ouStreetAddress']
+              State                           = $PSBoundParameters['ouState']
+            }
+            $OUexists = New-ADOrganizationalUnit @parameters
           }
-
-          Write-Verbose -Message 'Add GPO-link to corresponding OU'
-          $parameters = @{
-            GUID        = $CurrentNewGPO.Id
-            Target      = $PSBoundParameters['gpoLinkPath']
-            LinkEnabled = 'Yes'
-            Server      = $dcServer
-          }
-          New-GPLink @parameters
-
-          # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-          # Adding settings
-          #Write-Host "Setting Screen saver timeout to 15 minutes"
-          #Set-GPRegistryValue -Name $gpoName -key "HKCU\Software\Policies\Microsoft\Windows\Control Panel\Desktop" -ValueName ScreenSaveTimeOut -Type String -value 900
-
-          #Write-Host "Enable Screen Saver"
-          #Set-GPRegistryValue -Name $gpoName -key "HKCU\Software\Policies\Microsoft\Windows\Control Panel\Desktop" -ValueName ScreenSaveActive -Type String -value 1
-
-          #Write-Host "Disable Desktop Cleanup Wizzard"
-          #Set-GPRegistryValue -Name $gpoName -key "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -ValueName NoDesktopCleanupWizard -Type Dword -value 1
-
-          #Write-Host "Remove MyMusic from Start Menu"
-          #Set-GPRegistryValue -Name $gpoName -key "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -ValueName NoStartMenuMymusic -Type Dword -value 1
         }
-        else
+        catch
         {
-          Write-Verbose -Message ('{0} Policy already exist. Skipping.' -f $gpoName)
+          throw
         }
-    }
+
+        # Remove "Account Operators" and "Print Operators" built-in groups from OU. Any unknown/UnResolvable SID will be removed.
+        Start-AdCleanOU -LDAPPath $ouNameDN -RemoveUnknownSIDs
+
+        if($PSBoundParameters['CleanACL'])
+        {
+            Remove-SpecificACLandEnableInheritance -LDAPpath $ouNameDN
+        }
+      }
+
     End
     {
-        Write-Verbose -Message ('Function New-DelegateAdGpo Finished creating {0} GPO' -f $gpoName)
+
+        Write-Verbose -Message ('Function New-DelegateAdOU finished {0}' -f $ouNameDN)
         Write-Verbose -Message ''
         Write-Verbose -Message '--------------------------------------------------------------------------------'
         Write-Verbose -Message ''
-
-        return $CurrentNewGPO
+        return $OUexists
     }
 }
