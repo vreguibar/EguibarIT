664e247cf9a967d4460ca7b69b591bca21d9ecf2
Vicente Rodriguez Eguibar <38666475+vreguibar@users.noreply.github.com>
2021-09-08 09:42:08 +0200

    pipeline and publishing test

---
+++ Publish.ps1
@@ -0,0 +1,42 @@
+param (
+    [string] $version,
+    [string] $preReleaseTag,
+    [string] $apiKey
+)
+
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$srcPath = "$scriptPath\src";
+Write-Host "Proceeding to publish all code found in $srcPath"
+
+$outFile = "$scriptPath\BuildUtils\BuildUtils.psm1"
+if (Test-Path $outFile) 
+{
+    Remove-Item $outFile
+}
+
+if (!(Test-Path "$scriptPath\BuildUtils")) 
+{
+    New-Item "$scriptPath\BuildUtils" -ItemType Directory
+}
+
+$ScriptFunctions = @( Get-ChildItem -Path $srcPath\*.ps1 -ErrorAction SilentlyContinue -Recurse )
+$ModulePSM = @( Get-ChildItem -Path $srcPath\*.psm1 -ErrorAction SilentlyContinue -Recurse )
+foreach ($FilePath in $ScriptFunctions) {
+    $Results = [System.Management.Automation.Language.Parser]::ParseFile($FilePath, [ref]$null, [ref]$null)
+    $Functions = $Results.EndBlock.Extent.Text
+    $Functions | Add-Content -Path $outFile
+}
+foreach ($FilePath in $ModulePSM) {
+    $Content = Get-Content $FilePath
+    $Content | Add-Content -Path $outFile
+}
+"Export-ModuleMember -Function * -Cmdlet *" | Add-Content -Path $outFile
+
+# Now replace version in psd1
+
+$fileContent = Get-Content "$scriptPath\src\BuildUtils.psd1.source"
+$fileContent = $fileContent -replace '{{version}}', $version
+$fileContent = $fileContent -replace '{{preReleaseTag}}', $preReleaseTag 
+Set-Content "$scriptPath\BuildUtils\BuildUtils.psd1" -Value $fileContent  -Force
+
+Publish-Module -Path $scriptPath\BuildUtils -NuGetApiKey $apiKey -Verbose -Force
