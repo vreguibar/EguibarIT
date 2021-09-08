f88a792087a57426666bd2d3a4c06c63b8dd1de7
Vicente Rodriguez Eguibar <38666475+vreguibar@users.noreply.github.com>
2021-09-08 21:27:15 +0200

    bump

--- Publish.ps1
+++ Publish.ps1
@@ -4,28 +4,34 @@ param (
 )
 
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+
+
 $srcPath = "$scriptPath\src";
 Write-Host "----------------------------------------------------"
 Write-Host "Script Path $scriptPath"
 Write-Host "----------------------------------------------------"
 Write-Host "Source Path $srcPath"
 
-$outFile = "$scriptPath\EguibarIT\EguibarIT.psm1"
+$outFile = "srcPath\EguibarIT.psm1"
 if (Test-Path $outFile) {
     Remove-Item $outFile
 }
 
-if (-Not(Test-Path "$scriptPath\EguibarIT")) {
-    New-Item "$scriptPath\EguibarIT" -ItemType Directory
+if (-Not(Test-Path "$scriptPath\src")) {
+    New-Item "$scriptPath\src" -ItemType Directory
 }
 
-$ScriptFunctions = @( Get-ChildItem -Path $srcPath\*.ps1 -ErrorAction SilentlyContinue -Recurse )
-$ModulePSM = @( Get-ChildItem -Path $srcPath\*.psm1 -ErrorAction SilentlyContinue -Recurse )
+$ScriptFunctions = @( Get-ChildItem -Path $scriptPath\*.ps1 -ErrorAction SilentlyContinue -Recurse )
+$ModulePSM = @( Get-ChildItem -Path $scriptPath\*.psm1 -ErrorAction SilentlyContinue -Recurse )
+
+
 foreach ($FilePath in $ScriptFunctions) {
     $Results = [System.Management.Automation.Language.Parser]::ParseFile($FilePath, [ref]$null, [ref]$null)
     $Functions = $Results.EndBlock.Extent.Text
     $Functions | Add-Content -Path $outFile
 }
+
+
 foreach ($FilePath in $ModulePSM) {
     $Content = Get-Content $FilePath
     $Content | Add-Content -Path $outFile
@@ -41,11 +47,11 @@ $FileContent = Import-PowerShellDataFile -Path "$scriptPath\EguibarIT.psd1" -Ver
 [version]$NewVersion = '{0}.{1}.{2}' -f $Version.Major, $Version.Minor, ($Version.Build + 1) 
 Write-Host "New Version is: $NewVersion"
 $Splat = @{
-    Path          = "$scriptPath\EguibarIT.psd1"
+    Path          = "$srcPath\EguibarIT.psd1"
     ModuleVersion = $NewVersion
     Prerelease    = $preReleaseTag
     Verbose       = $true
 }
 Update-ModuleManifest @Splat
 
-Publish-Module -Path $scriptPath\EguibarIT -NuGetApiKey $apiKey -Force -Verbose
+Publish-Module -Path $srcPath -NuGetApiKey $apiKey -Force -Verbose
