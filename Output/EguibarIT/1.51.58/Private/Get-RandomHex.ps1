6c06a62d57a80bc602628baba5ff602c0735b910
Vicente Rodriguez Eguibar <38666475+vreguibar@users.noreply.github.com>
2022-05-19 13:30:53 +0200

    Delete Output/EguibarIT/1.51.58 directory

--- Output/EguibarIT/1.51.58/Private/Get-RandomHex.ps1
+++
@@ -1,9 +0,0 @@
-Function Get-RandomHex {
-param ([int]$Length)
-    $Hex = '0123456789ABCDEF'
-    [string]$Return = $null
-    For ($i=1;$i -le $length;$i++) {
-        $Return += $Hex.Substring((Get-Random -Minimum 0 -Maximum 16),1)
-    }
-    Return $Return
-}
