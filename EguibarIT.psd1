9c660f55d364cc8fbc2b2d99cec00af509066210
vreguibar <Vicente@EguibarIT.com>
2024-02-13 17:11:36 +0100

    Increase version module

--- EguibarIT.psd1
+++ EguibarIT.psd1
@@ -12,7 +12,7 @@
     RootModule           = 'EguibarIT.psm1'
 
     # Version number of this module.
-    ModuleVersion        = '1.52.65'
+    ModuleVersion        = '1.06.00'
 
     # Supported PSEditions
     CompatiblePSEditions = 'Desktop'
--- EguibarIT.psm1
+++ EguibarIT.psm1
@@ -1,14 +1,15 @@
 #Get public and private function definition files.
-$Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1  -ErrorAction SilentlyContinue -Recurse )
-$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue -Recurse )
 $Classes = @( Get-ChildItem -Path $PSScriptRoot\Classes\*.ps1 -ErrorAction SilentlyContinue -Recurse )
-$Enums   = @( Get-ChildItem -Path $PSScriptRoot\Enums\*.ps1   -ErrorAction SilentlyContinue -Recurse )
+$Enums = @( Get-ChildItem -Path $PSScriptRoot\Enums\*.ps1 -ErrorAction SilentlyContinue -Recurse )
+$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue -Recurse )
+$Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue -Recurse )
+
 
 #Dot source the files
 Foreach ($import in @($Classes + $Enums + $Private + $Public)) {
     Try {
         . $import.fullname
-       # Write-Warning $import.fullname
+        # Write-Warning $import.fullname
     } Catch {
         Write-Error -Message "Failed to import functions from $($import.Fullname): $_"
     }
