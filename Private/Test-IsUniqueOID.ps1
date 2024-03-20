6c4f1d07956d1a0be6c42238dde6dc08fb0111f7
vreguibar <Vicente@EguibarIT.com>
2024-03-20 16:34:52 +0100

    Add sections, verbosing and formatting fixes

--- Private/Test-IsUniqueOID.ps1
+++ Private/Test-IsUniqueOID.ps1
@@ -23,40 +23,85 @@ Function Test-IsUniqueOID {
     #>
     [CmdletBinding(ConfirmImpact = 'low')]
     [OutputType([System.Boolean])]
+
     param (
-        [Parameter(Mandatory = $true)]
-        [string]$cn,
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, ValueFromRemainingArguments = $False,
+            HelpMessage = 'Specifies the Common Name (CN) of the Certificate Template',
+            Position = 0)]
+        [string]
+        $cn,
 
-        [Parameter(Mandatory = $true)]
-        [string]$TemplateOID,
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, ValueFromRemainingArguments = $False,
+            HelpMessage = 'Specifies the OID (Object Identifier) of the Certificate Template',
+            Position = 1)]
+        [string]
+        $TemplateOID,
 
-        [Parameter(Mandatory = $true)]
-        [string]$Server,
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, ValueFromRemainingArguments = $False,
+            HelpMessage = 'Specifies the Active Directory server to query',
+            Position = 2)]
+        [string]
+        $Server,
 
-        [Parameter(Mandatory = $true)]
-        [string]$ConfigNC
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, ValueFromRemainingArguments = $False,
+            HelpMessage = 'Specifies the Configuration Naming Context (ConfigNC) to search for the Certificate Template.',
+            Position = 3)]
+        [string]
+        $ConfigNC
     )
 
-    try {
-        # Query Active Directory for the Certificate Template
-        $Search = Get-ADObject -Server $Server `
-            -SearchBase "CN=OID,CN=Public Key Services,CN=Services,$ConfigNC" `
-            -Filter { cn -eq $cn -and msPKI-Cert-Template-OID -eq $TemplateOID } -ErrorAction Stop
+    Begin {
+        Write-Verbose -Message '|=> ************************************************************************ <=|'
+        Write-Verbose -Message (Get-Date).ToShortDateString()
+        Write-Verbose -Message ('  Starting: {0}' -f $MyInvocation.Mycommand)
+        Write-Verbose -Message ('Parameters used by the function... {0}' -f (Get-FunctionDisplay $PsBoundParameters -Verbose:$False))
 
-        # If the Certificate Template is found, it's not unique
-        if ($Search) {
-            Write-Verbose "Certificate Template with OID '$TemplateOID' already exists."
+        ##############################
+        # Variables Definition
+
+        $Splat = [Hashtable]::New()
+
+        $SearchBase = 'CN=OID,CN=Public Key Services,CN=Services,{0}' -f $ConfigNC
+        $Filter = '{ cn -eq {0} -and msPKI-Cert-Template-OID -eq {1} }' -f $cn, $TemplateOID
+
+    } #end Begin
+
+    Process {
+        try {
+            # Query Active Directory for the Certificate Template
+            $Splat = @{
+                Server      = $Server
+                SearchBase  = $SearchBase
+                Filter      = $filter
+                ErrorAction = Stop
+            }
+            $Search = Get-ADObject @Splat
+
+            # If the Certificate Template is found, it's not unique
+            if ($Search) {
+
+                Write-Verbose -Message 'Certificate Template with OID {0} already exists.' -f $TemplateOID
+                return $false
+
+            } else {
+
+                Write-Verbose -Message 'Certificate Template with OID {0} is unique.' -f $TemplateOID
+                return $true
+
+            } #end If
+
+        } catch {
+            # Handle errors and provide verbose output
+            Get-CurrentErrorToDisplay -CurrentError $error[0]
+            Write-Verbose 'An error occurred while checking the Certificate Template OID uniqueness.'
             return $false
-        }
-        else {
-            Write-Verbose "Certificate Template with OID '$TemplateOID' is unique."
-            return $true
-        } #end If
-    }
-    catch {
-        # Handle errors and provide verbose output
-        Get-CurrentErrorToDisplay -CurrentError $error[0]
-        Write-Verbose 'An error occurred while checking the Certificate Template OID uniqueness.'
-        return $false
-    } #end Try-Catch
+        } #end Try-Catch
+    } #end Process
+
+    End {
+        Write-Verbose -Message "Function $($MyInvocation.InvocationName) finished checking the Certificate Template OID uniqueness."
+        Write-Verbose -Message ''
+        Write-Verbose -Message '--------------------------------------------------------------------------------'
+        Write-Verbose -Message ''
+    } #end End
 } #end Function
