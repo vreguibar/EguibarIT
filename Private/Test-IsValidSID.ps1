9516416008659c6f17e4c48436c7692592222155
vrguibar <vicente@EguibarIT.com>
2024-06-14 16:48:13 +0200

    Improve RegEx validation

--- Private/Test-IsValidSID.ps1
+++ Private/Test-IsValidSID.ps1
@@ -23,14 +23,15 @@ function Test-IsValidSID {
                 Eguibar Information Technology S.L.
                 http://www.eguibarit.com
     #>
-    [CmdletBinding(ConfirmImpact = 'Low', SupportsShouldProcess = $true)]
+    [CmdletBinding(ConfirmImpact = 'Low', SupportsShouldProcess = $false)]
     [OutputType([bool])]
+
     param
     (
         [Parameter(Mandatory = $true,
             ValueFromPipeline = $true,
             ValueFromPipelineByPropertyName = $true,
-            ValueFromRemainingArguments = $false,
+            ValueFromRemainingArguments = $true,
             HelpMessage = 'String to be validated as SID',
             Position = 0)]
         [ValidateNotNullOrEmpty()]
@@ -38,28 +39,60 @@ function Test-IsValidSID {
         [string]
         $ObjectSID
     )
+
     Begin {
         # Define DN Regex
-        $SidRegex = [RegEx]::new('^S-1-[0-59]-\d{2}-\d{8,10}-\d{8,10}-\d{8,10}-[1-9]\d{3}')
+        #$SidRegex = [RegEx]::new('^S-1-[0-59]-\d{2}-\d{8,10}-\d{8,10}-\d{8,10}-[1-9]\d{3}')
+        $SidRegex = [RegEx]::new('^S-1-(0|1|2|3|4|5|59)-\d+(-\d+)*$')
+
     } #end Begin
+
     Process {
+        # try RegEx
         Try {
-            # Use ShouldProcess to confirm the operation
-            if ($PSCmdlet.ShouldProcess($ObjectDN, 'Validate objectSID')) {
-                # Perform the actual validation
-                $isValid = $ObjectSID -match $SidRegex
+            if ($SIDRegex.IsMatch($SID)) {
+
+                $isValid = $tue
 
                 # Provide verbose output
                 if ($PSCmdlet.MyInvocation.BoundParameters['Verbose']) {
-                    Write-Verbose "objectSID validation result: $isValid"
+                    Write-Verbose -Message ('The SID {0} is valid.' -f $SID)
                 } #end If
-            } #end If
+            } else {
+                $isValid = $false
+
+                # Provide verbose output
+                if ($PSCmdlet.MyInvocation.BoundParameters['Verbose']) {
+                    Write-Verbose -Message ('[WARNING] The SID {0} is NOT valid!.' -f $SID)
+                } #end If
+            }
+
         } catch {
             # Handle exceptions gracefully
-            Write-Error "An error occurred: $_"
+            Write-Error -Message ('An error occurred when validating the SID: {0}' -f $_)
         } #end Try-Catch
+
+        <#
+        # try Native SID
+        Try {
+            # Perform the actual validation
+            [System.Security.Principal.SecurityIdentifier]$sid = $Sid
+            $isValid = $True
+
+            # Provide verbose output
+            if ($PSCmdlet.MyInvocation.BoundParameters['Verbose']) {
+                Write-Verbose "objectSID validation result by [SecurityIdentifier]: $isValid"
+            } #end If
+
+        } catch {
+            # Handle exceptions gracefully
+            Write-Error "An error occurred on [SecurityIdentifier] comparison: $_"
+        } #end Try-Catch
+         #>
     } #end Process
+
     end {
         return $isValid
     } #end End
+
 } #end Function
