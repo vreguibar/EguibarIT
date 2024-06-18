27092d0e3dd4d006b02e0305aff305ba47c5f718
vreguibar <vicente@EguibarIT.com>
2024-06-18 09:01:32 +0200

    Fix casing comparison

--- Enums/Enum.Variables.ps1
+++ Enums/Enum.Variables.ps1
@@ -1,4 +1,5 @@
-﻿$Variables = [ordered] @{
+﻿[Hashtable]$Variables = [hashtable]::New([StringComparer]::OrdinalIgnoreCase)
+$Variables = [ordered] @{
 
     # Active Directory DistinguishedName
     AdDN                       = $null
@@ -13,10 +14,10 @@
     DnsFqdn                    = $null
 
     # Hashtable containing the mappings between SchemaExtendedRights and GUID's
-    ExtendedRightsMap          = $null
+    ExtendedRightsMap          = [hashtable]::New([StringComparer]::OrdinalIgnoreCase)
 
     # Hashtable containing the mappings between ClassSchema/AttributeSchema and GUID's
-    GuidMap                    = $null
+    GuidMap                    = [hashtable]::New([StringComparer]::OrdinalIgnoreCase)
 
     # Naming Contexts
     namingContexts             = $null
@@ -31,7 +32,7 @@
     SchemaNamingContext        = $null
 
     # Well-Known SIDs
-    WellKnownSIDs              = $null
+    WellKnownSIDs              = [hashtable]::New([StringComparer]::OrdinalIgnoreCase)
 }
 
 $Splat = @{
